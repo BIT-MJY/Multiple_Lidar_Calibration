eb3b9e5410a1d7a5e733786ff42e421b7d535cd3
Junyi Ma <56945855+BIT-MJY@users.noreply.github.com>
2021-09-05 20:13:19 +0800

    Update select_pcd.cpp
    Add ground removal.

--- save_pcd/src/select_pcd.cpp
+++ save_pcd/src/select_pcd.cpp
@@ -66,28 +66,104 @@ std::mutex mBuf;
 std::vector< pcl::PointCloud<pcl::PointXYZ>::Ptr> Vec16;
 std::vector< pcl::PointCloud<pcl::PointXYZ>::Ptr> Vec32;
 pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_show(new pcl::PointCloud<pcl::PointXYZ>);
-
+pcl::PointCloud<pcl::PointXYZ>::Ptr  cloud_filtered_show(new pcl::PointCloud<pcl::PointXYZ>());
 
 
 bool finishLable = false;
 bool nextLable = true;
-
+bool remove_ground_points = true;
 
 
 std::string filename_pt = "  ";
 std::string filename_pt_save = "  ";
 std::string filesuffix;
 std::string num;
+double z_measured = 0;
+
+enum AXIS
+{
+    X=0,
+    Y,
+    Z
+};
+
+void planeFittingWithAxis(pcl::PointCloud<pcl::PointXYZ>::Ptr& laserCloud, pcl::PointCloud<pcl::PointXYZ>::Ptr& fittedPlane
+                                                    , Eigen::VectorXf& coefficients_vec, double z_measured_)
+{
+
+    fittedPlane->points.clear();
+    pcl::PointIndices::Ptr tmpinliers(new pcl::PointIndices);
+    // 把-z+0.2以下的点云拿到
+    // TODO: 直通滤波(pcl::PassThrough<pcl::PointXYZ> pass)
+
+    pcl::PointCloud<pcl::PointXYZ>::Ptr  cloud_low(new pcl::PointCloud<pcl::PointXYZ>());
+    pcl::PointCloud<pcl::PointXYZ>::Ptr  cloud_up(new pcl::PointCloud<pcl::PointXYZ>());
+
+    for (int ind =0; ind <laserCloud->points.size(); ind ++)
+    {
+        if (laserCloud->points[ind].z <( -z_measured_ + 0.2))
+        {
+            cloud_low->points.push_back(laserCloud->points[ind]);
+        }
+        else 
+        {
+            cloud_up->points.push_back(laserCloud->points[ind]);                   
+        }
+    }
+
+
+    pcl::SampleConsensusModelPerpendicularPlane<pcl::PointXYZ>::Ptr
+            model( new pcl::SampleConsensusModelPerpendicularPlane<pcl::PointXYZ> (cloud_low)  );
+
+    model->setAxis(Eigen::Vector3f(0.0,0.0,1.0));//setAxis用于设置所搜索平面垂直的轴
+    double dgree = 5.0;
+    double max = 10000;
+    double distance = 0.1;
+
+    model->setEpsAngle(pcl::deg2rad(dgree));//setEpsAngle用于设置待检测的平面模型和上述轴的最大角度
+    pcl::RandomSampleConsensus<pcl::PointXYZ> ransac(model);
+    ransac.setMaxIterations(max);
+    ransac.setDistanceThreshold(distance);
+    ransac.computeModel();
+    ransac.getInliers(tmpinliers->indices);
+    ransac.getModelCoefficients(coefficients_vec);
+
+    for (int i=0; i<cloud_low->points.size(); i++)
+    {
+        bool inlierflag = false;
+        for (int j=0; j<tmpinliers->indices.size(); j++)
+        {
+                if ( i == tmpinliers->indices[j]  )
+                {
+                    inlierflag = true;
+                    break;
+                }
+        }
+        if (!inlierflag)
+        {
+            fittedPlane->points.push_back(  cloud_low->points[i]  );    
+        }   
+    }
+
+    for (int j=0; j<cloud_up->points.size(); j++)
+    {
+        fittedPlane->points.push_back(  cloud_up->points[j]  );    
+    }
+
+}
+
+
 
 
 void areapickingcallback(const pcl::visualization::AreaPickingEvent &event,void *userdata)
 {
+
     pcl::PointCloud<pcl::PointXYZ>::Ptr  secloud(new pcl::PointCloud<pcl::PointXYZ>());
     std::vector<int> indices;
     event.getPointsIndices(indices);
     pcl::IndicesPtr ind_plane=boost::make_shared<std::vector<int>>(indices);
     pcl::ExtractIndices<pcl::PointXYZ> extract;
-    extract.setInputCloud(cloud_show);                       //导入点云数据
+    extract.setInputCloud(cloud_show);                  
     extract.setIndices(ind_plane);                      //设置点云索引
     extract.setNegative(false);                  //设置为false，选择索引指向的点导出
     extract.filter(*secloud);                         //输出所选点云
@@ -98,7 +174,6 @@ void areapickingcallback(const pcl::visualization::AreaPickingEvent &event,void 
 	}
 
 
-    // 2.0 version 决定是否使用本次标注的点云
     std::cout<<"是否使用本次框选的点云数据?: y(Yes)/n(No): "<<std::endl;
     std::string save_char;
     std::cin>>save_char;
@@ -145,6 +220,17 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "select_pcd");
     ros::NodeHandle nh;
     ros::param::get("~suffix", filesuffix);
+    ros::param::get("~remove_ground_points", remove_ground_points);
+    ros::param::get("~z_measured", z_measured);
+
+    if (remove_ground_points)
+    {
+        std::cout<<"使用地面点过滤，建议对安装相对地面较为平行（不超过5度）的雷达进行使用"<<std::endl;
+    }
+    else 
+    {
+        std::cout<<"不使用地面点过滤"<<std::endl;
+    }
 
     for (int k=0; k<100; k++)
     {
@@ -172,17 +258,29 @@ int main(int argc, char **argv)
         std::vector<int> indices_nonan;
         pcl::removeNaNFromPointCloud(*cloud_show,*cloud_show, indices_nonan);
 
+        Eigen::VectorXf coefficients;
+        planeFittingWithAxis(cloud_show, cloud_filtered_show, coefficients, z_measured);
+
         viewer->setBackgroundColor(0,0,0);
-        viewer->addPointCloud(cloud_show,"forCalib");
         viewer->setCameraPosition(5, 5, 5, 0, 0, 0, 0);
+        std::cout<<cloud_show->points.size()<<std::endl;
+        if (remove_ground_points)
+        {
+            cloud_show->points.clear();
+            for (int k = 0; k<cloud_filtered_show->points.size(); k++)
+            {
+                cloud_show->points.push_back(cloud_filtered_show->points[k]);
+            }
+        }
+        viewer->addPointCloud(cloud_show,"forCalib");
+        std::cout<<cloud_filtered_show->points.size()<<std::endl;
         viewer->registerAreaPickingCallback(areapickingcallback, (void*)&cloud_show);
-        // while (!viewer->wasStopped ())
         while (!finishLable)
         {
             viewer->spinOnce (100);
             // boost::this_thread::sleep (boost::posix_time::microseconds (100000));
         }
-        // thisLable = true;
+        cloud_show->points.clear();
         if (!nextLable)
         {
             k -= 1;
@@ -190,16 +288,6 @@ int main(int argc, char **argv)
         
 
     }
-
-
-        
-
-    // while(ros::ok())
-    // {
-    //     ros::spinOnce();
-    //     savetotalpcd();
-    // }
-
 
     ros::spin();
 
